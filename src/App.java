887816bba438b1fb0df1b73a32b260bb5722aca4
ThaisMK <thais.konishi@gmail.com>
2022-07-13 10:44:11 -0300

    fazendo testes

--- src/App.java
+++ src/App.java
@@ -12,7 +12,17 @@ original.add(Map.of("nome", "Maria", "nascimento", "24-07-1988 23:02:41"));
 original.add(Map.of("nome", "Ana", "nascimento", "03:58:26 14-02-1983"));
 original.add(Map.of("nome", "Pedro", "nascimento", "08:03:07 1989-11-02"));
 
+String[] datas = new String[]{"1985-12-11 12:10:33", "24-07-1988 23:02:41","03:58:26 14-02-1983","08:03:07 1989-11-02" };
 
+for (String data : datas) {
+    if(data.matches("^\\d{4}")){
+        System.out.println(LocalDateTime.parse(data));
+            }else{
+                System.out.println(LocalDateTime.parse(data, DateTimeFormatter.ofPattern("dd//MM/yyyy")));
+            }
+}
+
+System.out.println(LocalDateTime.parse("1985-12-11 12:10:33", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))); 
 System.out.println(original.get(0).toString());
     }
 }
